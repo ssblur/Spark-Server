5f3fd6bd0ceb14a9d594d95e22542b0f9e6de6df
Patrick Emery <emeryp15@students.ecu.edu>
2020-02-04 15:10:52 -0500

    Added login skeleton, initialized node project.
    Signed-off-by: Patrick Emery <emeryp15@students.ecu.edu>

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+/**
+ * Spark Server Landing
+ * The portal through which Spark server features are accessed.
+ * This package will include individual components, as well as a built in server
+ * @author Patrick Emery
+ */
+
+/**
+ * index.main
+ * Arguments:
+ *      args - Array (String), The command line arguments provided.
+ * Returns:
+ *      undefined / None
+ */
+function main( args ){
+
+}
+
+/**
+ * index.index
+ * Arguments:
+ *      args - Object (Dict<String : any>), The GET and POST arguments posited
+ *          by a given request.
+ * Returns:
+ *      undefined / None
+ */
+function index( args ){
+
+}
