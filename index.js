3b2ee1942b156ca3197fb371970df4dda3374c16
Patrick Emery <emeryp15@students.ecu.edu>
2020-02-12 17:49:18 -0500

    Simplified run logic, as this will never be run as a CGI script.
    Signed-off-by: Patrick Emery <emeryp15@students.ecu.edu>

--- index.js
+++ index.js
@@ -14,43 +14,12 @@
  * @return void
  */
 function main( args ){
-
-}
-
-/**
- * The main function when called from a recognized web server or with the web
- * setting set.
- * Used internally by the internal node web server.
- * @param Object<Dict<String : any>>    args    The POST arguments posited
- *          by a given request, if any.
- * @return  String      The headers and content for a given request.
- */
-function index( args ){
-
+    global route;
+    const express = require( "express" )
+    const route =
 }
 
 // Only run if this script is executed directly.
 if( require.main === module ){
-    var run = false;
-    // Checks if the "--server" or "-s" have been passed to the script.
-    process.argv.forEach( argument =>{
-        if( argument.startsWith("--") ){
-            if( argument=="--server" ){
-                main( process.argv );
-                run = true;
-            }
-        } else if( argument.startsWith("-") ){
-            if( argument.includes("s") ){
-                main( process.argv );
-                run = true;
-            }
-        }
-    })
-
-    // If no alternate run method was defined, this just runs the script as if
-    // it were hosted on a web server.
-    if( !run ){
-        console.log( "Module run directly without server arg. Running as script.")
-        print( index( {} ) )
-    }
+    main( process.argv );
 }
