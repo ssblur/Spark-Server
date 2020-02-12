be84a98be714f3bd9dd3017686d628d61cb3cfc9
Patrick Emery <emeryp15@students.ecu.edu>
2020-02-12 18:41:14 -0500

    Added default pages for login and verify.
    Signed-off-by: Patrick Emery <emeryp15@students.ecu.edu>

--- index.js
+++ index.js
@@ -7,16 +7,42 @@
  */
 
 
+
 const express = require( 'express' );
 const session = require( 'express-session' );
 const parser = require( 'body-parser' );
 const router = express.Router();
 const app = express();
 
-app.use( session( {secret: '', saveUninitialized: true, resave: true} ) );
-app.use( parser.json());
-app.use( bodyParser.urlencoded( {extended: true} ) );
-app.use( express.static(__dirname + '/pages') );
+const lib = require( "./lib" );
+var config;
+lib.config.load( __dirname ).then( data => {
+    config = data;
+    // Registering middleware.
+    app.use( session( {secret: config.secret, saveUninitialized: true, resave: true} ) );
+    app.use( parser.json() );
+    app.use( parser.urlencoded( {extended: true} ) );
+
+    // Defining an error handler.
+    app.use( ( err, req, res, next ) => {
+
+    });
+
+    const default_page = ( req, res ) => {};
+
+    // A request for dispatching a login code to a user.
+    app.post( "/login", lib.login.login || default_page );
+    app.get( "/login", lib.defaults.login_get || default_page );
+
+    // A request for verifying a login with a verification code.
+    app.post( "/verify", lib.login.verify || default_page );
+    app.get( "/verify", lib.defaults.verify_get || default_page );
+
+    // A placeholder request for icon submission.
+    app.put( "/submit_icon", lib.icon.put || default_page );
+
+    app.listen( 8080, () => { console.log( "Application loaded on port 8080." ); } );
+});
 
 /**
  * The main function when called from a command line.
