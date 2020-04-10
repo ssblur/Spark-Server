7b1a41cdb5297f206cc549efe7a17b1f31cbfc92
Patrick Emery <patrick.emery@outlook.com>
2020-04-10 08:12:37 -0400

    Users now get account data when logging in.
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

--- setup.sql
+++ setup.sql
@@ -1,1 +1,2 @@
 create table verification_table (code varchar(6), exp_date datetime, email_phone varchar(70), constraint verification_table_pk primary key (email_phone));
+create table profile_information (uuid char(36) not null, picture_id varchar(36), email_phone text not null, user_name text not null, contacts mediumblob, constraint profile_information_pk primary key (uuid));
