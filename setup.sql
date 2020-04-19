03194018b0f47ca2c2bb90061a4645a7093517ef
Patrick Emery <patrick.emery@outlook.com>
2020-04-19 15:41:06 -0400

    Fixed typo in setup.sql
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

--- setup.sql
+++ setup.sql
@@ -4,4 +4,4 @@ create table chat (uuid char(36), picture_id char(36), chat_name text not null, 
 create table chat_members (cuuid char(36), uuid char(36), constraint chat_members_pk primary key (cuuid, uuid));
 create table chat_messages (uuid char(36), message_type tinyint, content text, message_sent datetime, sender char(36), constraint chat_messages_pk primary key (uuid, message_sent));
 create table pictures (uuid char(36), content mediumblob, constraint pictures_pk primary key (uuid));
-create table unread_messages (cuuid char(36), message_sent datetime, uuid char(36) constraint unread_messages_pk primary key (cuuid, message_send uuid));
+create table unread_messages (cuuid char(36), message_sent datetime, uuid char(36) constraint unread_messages_pk primary key (cuuid, message_sent));
